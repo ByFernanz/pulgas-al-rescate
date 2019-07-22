c13eef5646c1375ebd7eb6103fe8743d1630f609
ByFernanz <byfernanz@gmail.com>
2019-07-21 22:13:54 -0400

    commit generico

--- compilar.sh
+++ compilar.sh
@@ -2,5 +2,5 @@ if [ -d pulgas-al-rescate ];
 then
 	rm -R pulgas-al-rescate
 fi
-ficdown --format html --in pulgas-al-rescate.md --out pulgas-al-rescate --author "Billy Fernández" --language es
+ficdown --format html --in pulgas-al-rescate.md --out pulgas-al-rescate --author "Billy Fernández" --template template --language es
 chromium-browser pulgas-al-rescate/index.html
